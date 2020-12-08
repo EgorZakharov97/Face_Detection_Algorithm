20beec40622574b01f3d51c1d9a317253de2ca8e
BrittBarber <57416959+BrittBarber@users.noreply.github.com>
2020-12-08 00:12:17 -0600

    Update TestFaceDetection.py
    Added test for original algorithm and modified it to use the FaceDetection object

--- TestFaceDetection.py
+++ TestFaceDetection.py
@@ -1,7 +1,9 @@
-from FaceDetection import testImage
+from FaceDetection import FaceDetector
+
+detector = FaceDetector()
 
 
-def findAverageD(fileNames, path):
+def findAverageD(fileNames, path, detect, save):
     filelist = []
 
     with open(fileNames) as libfile:
@@ -9,20 +11,32 @@ def findAverageD(fileNames, path):
             name = path + line.strip()
             filelist.append(name)
 
-    dmin = 0
+    d = detector.detectFace(filelist[0], detectEdges = detect, saveData = save)
 
-    for image in range( len(filelist) ):
-        d, index = testImage(filelist[image])
-        dmin += d
-
-    return dmin/len(filelist)
+    for image in range( 1 ,len(filelist), +1 ):
+        d += detector.detectFace(filelist[image], detectEdges = False, saveData = True)
+    
+    return d/len(filelist)
+    
     
 
-
+#find original dmin and dmax
+originalDMin = findAverageD('duplicates.txt', './duplicates/', detect = False, save = False)
+originalDMax = findAverageD('singles.txt', './singles/', detect = False, save = True)
+originalDifference = originalDMax - originalDMin
 
 #find modified dmin and dmax
-modifiedDMin = findAverageD('duplicates.txt', './duplicates/')
-modifiedDMax = findAverageD('singles.txt', './singles/')
-
+modifiedDMin = findAverageD('duplicates.txt', './duplicates/', detect = True, save = False)
+modifiedDMax = findAverageD('singles.txt', './singles/', detect = False, save = True)
 modifiedDifference = modifiedDMax - modifiedDMin
+
+print('The original dmin is: ' + str(originalDMin))
+print('The original dmax is: ' + str(originalDMax))
+print('The original difference is: ' + str(originalDifference))
+print('The modified dmin is: ' + str(modifiedDMin))
+print('The modified dmax is: ' + str(modifiedDMax))
 print('The modified difference is: ' + str(modifiedDifference))
+
+
+
+
